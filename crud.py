923a1bd5a31237ebf7e0bde59d9a6d6709558d45
Mateus Henrique <mateus.queroz@aluno.senai.br>
2025-09-30 12:41:28 -0300

    função

--- crud.py
+++ crud.py
@@ -29,3 +29,28 @@ def listar_aluno():
         finally:
             cursor.close()
             conexao.close()
+
+def atualizar_alunos(id_aluno, nova_idade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+            "UPDATE alunos SET idade = %s WHERE id = %s",
+            (nova_idade, id_aluno)
+            )
+        except Exception as erro:
+            print(f"Erro ao atualizar um aluno: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+def deletar_aluno(id_aluno):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "DELETE FROM aluno WHERE id = %s", (id_aluno,)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"erro ao deletar aluno:")
